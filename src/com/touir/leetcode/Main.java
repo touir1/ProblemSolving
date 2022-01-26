378ec12e6f8175aec7d36ddf299c1d958925014e
Mohamed Ali Touir <touir.mat@gmail.com>
2022-01-26 01:08:48 +0100

    problem 4

--- src/com/touir/leetcode/Main.java
+++ src/com/touir/leetcode/Main.java
@@ -3,7 +3,7 @@ package com.touir.leetcode;
 public class Main {
 
 	public static void main(String[] args) {
-		Solution solution = new Solution941();
+		Solution solution = new Solution5();
 
 		solution.test();
 	}
---
+++ src/com/touir/leetcode/Solution5.java
@@ -0,0 +1,42 @@
+package com.touir.leetcode;
+
+public class Solution5 extends Solution {
+
+	// problem: https://leetcode.com/problems/longest-palindromic-substring/
+
+	public String longestPalindrome(String s) {
+		if (s.length() <= 1)
+			return s;
+
+		int maxRight = s.length() - 1, left = 0, right = 0, mid = 0, bestLength = 1, bestLeft = 0, len = 0;
+
+		while (mid < s.length()) {
+			left = mid;
+			right = mid;
+
+			while (right < maxRight && s.charAt(right + 1) == s.charAt(right))
+				right++;
+
+			while (left > 0 && right < maxRight && s.charAt(left - 1) == s.charAt(right + 1)) {
+				left--;
+				right++;
+			}
+			len = right - left + 1;
+			if (bestLength < len) {
+				bestLength = len;
+				bestLeft = left;
+			}
+			mid++;
+		}
+		return s.substring(bestLeft, bestLeft + bestLength);
+	}
+
+	@Override
+	public void test() {
+		System.out.println("result: " + this.longestPalindrome("babad") + ", expected: aba or bab");
+		System.out.println("result: " + this.longestPalindrome("cbbd") + ", expected: bb");
+		System.out.println("result: " + this.longestPalindrome("aacabdkacaa") + ", expected: aca");
+		System.out.println("result: " + this.longestPalindrome("bb") + ", expected: bb");
+	}
+
+}
