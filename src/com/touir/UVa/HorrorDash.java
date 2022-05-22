c04e25eeb0c461d40a8d90ee180edad450f80d99
touir1 <touir.mat@gmail.com>
2022-05-22 21:15:07 +0100

    problem 11799 uva

---
+++ src/com/touir/UVa/HorrorDash.java
@@ -0,0 +1,37 @@
+// remove package before submitting
+package com.touir.UVa;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//change classname to Main before submitting
+public class HorrorDash {
+
+	/*
+	 * problem: 11799 - Horror Dash
+	 * 
+	 * url: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=608&page=show_problem&problem=2899
+	 */
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scan = new Scanner(new FileInputStream("./src/com/touir/UVa/input/HorrorDash.txt"));
+		//Scanner scan = new Scanner(System.in);
+		
+		int t = scan.nextInt();
+		
+		for(int i=1;i<=t;i++) {
+			int n = scan.nextInt();
+			int clownSpeed = 0;
+			while(n-- > 0) {
+				clownSpeed = Math.max(clownSpeed, scan.nextInt());
+			}
+
+			System.out.println("Case "+i+": "+clownSpeed);
+		}
+		
+		
+		scan.close();
+	}
+
+}
